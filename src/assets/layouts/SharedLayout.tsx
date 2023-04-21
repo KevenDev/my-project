752a9b25cdd96ca17894930ae9920901961e1031
Keven Teles <glevenkeven@gmail.com>
2023-04-21 14:49:45 -0300

    feat: layout shared

---
+++ src/assets/layouts/SharedLayout.tsx
@@ -0,0 +1,16 @@
+import Header from "../../components/Header";
+import {Outlet} from "react-router-dom"
+ 
+
+const SharedLayout = ()=>{
+  return(
+    <>
+    <div>
+      <Header/>
+      <Outlet/>
+    </div>
+    </>
+  )
+}
+
+export default SharedLayout;
