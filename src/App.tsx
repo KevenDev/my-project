1b0070f1d9a508246b030173ffad2d8e5bdafcc8
Keven Teles <glevenkeven@gmail.com>
2023-04-21 14:49:36 -0300

    feat: new route

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,17 @@ import { BrowserRouter,Routes,Route } from 'react-router-dom'
 import Home from './pages/Home'
 import History from './pages/History'
 import './App.css'
+import SharedLayout from './assets/layouts/SharedLayout'
 
 function App() {
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Home/>}> </Route>
-        <Route path='/history' element={<History/>}> </Route>
+        <Route path='/' element={<SharedLayout/>}>
+          <Route path='/' element={<Home/>}> </Route>
+          <Route path='/history' element={<History/>}> </Route>
+        </Route>
       </Routes>
     </BrowserRouter>
   )
