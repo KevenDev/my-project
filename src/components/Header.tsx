68216695dd8267336195156f1504a7bf37ca15a4
Keven Teles <glevenkeven@gmail.com>
2023-04-23 13:53:45 -0300

    feat: change icons

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,20 @@
-import logoRocketseat from "../assets/logoRocketseat.svg"
+import logo from "../assets/logo.svg"
+import timer from "../assets/timer.svg"
+import iconList from "../assets/iconList.svg"
 
 const Header= ()=>{
   return(
-    <nav>
-      <img src={logoRocketseat} alt="Logo ignite" />
-    </nav>
+  <nav>
+    <div className="h-20 p-4">
+      <nav className="flex justify-between">
+        <img src={logo} width={25} alt="Logo" />
+        <div className="flex justify-center gap-4"> 
+          <img src={timer} width={20} alt="Timer" />
+          <img src={iconList} width={20} alt="list" />
+         </div>
+      </nav>
+    </div>
+  </nav>
   )
 }
 
