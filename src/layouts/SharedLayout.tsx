22059ae40e9c841dca8f17a84e2d1374fc0dd2e5
Keven Teles <glevenkeven@gmail.com>
2023-04-23 13:54:28 -0300

    feat: mobile first

--- src/layouts/SharedLayout.tsx
+++ src/layouts/SharedLayout.tsx
@@ -4,12 +4,12 @@ import {Outlet} from "react-router-dom"
 
 const SharedLayout = ()=>{
   return(
-    <>
-    <div>
-      <Header/>
-      <Outlet/>
+    <div className="flex flex-col justify-center items-center h-screen">
+      <div className="bg-[#202024] rounded-lg h-1/2 w-[90%] ">
+        <Header/>
+        <Outlet/>
+      </div>
     </div>
-    </>
   )
 }
 
