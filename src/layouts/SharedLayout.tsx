9650f53dbfeb529b98773fed93a0b15c91cd418e
Keven Teles <glevenkeven@gmail.com>
2023-04-23 14:45:52 -0300

    feat: new size shared Layout

--- src/layouts/SharedLayout.tsx
+++ src/layouts/SharedLayout.tsx
@@ -5,7 +5,7 @@ import {Outlet} from "react-router-dom"
 const SharedLayout = ()=>{
   return(
     <div className="flex flex-col justify-center items-center h-screen">
-      <div className="bg-[#202024] rounded-lg h-1/2 w-[90%] ">
+      <div className="bg-[#202024] rounded-lg w-[calc(100vw-30px)] h-[60%] ">
         <Header/>
         <Outlet/>
       </div>
